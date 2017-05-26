d995f86b4525c2c75c1e62eebfc3be96664112df
colonelrascals <pdhildreth@gmail.com>
2017-05-26 16:48:52 -0500

    login hack fixed....thanks chris

--- src/client/pages/login.cljs
+++ src/client/pages/login.cljs
@@ -29,17 +29,21 @@
 (defn send-login!
   "Send the login information to the server."
   [username]
-    (socket/emit "update-name" {:user username
-                                :color client.state/chat-color}))
+  (socket/emit "update-name" {:user username
+                              :color client.state/chat-color
 
-(defn- on-form-submit [js-evt]
-  (.preventDefault js-evt)
-  (let [username (dom/get-value "nameInput")]
-    ;; TODO: more username validation here
-    (when (not= username "")
-      (reset! client.state/username username)
-      (send-login! username)
-      (aset js/location "hash" "#/lobby"))))
+                              (defn- valid-username? [name]
+                                (and (string? name)
+                                     (not (str/blank? name))
+                                     (= -1 (.indexOf name "<"))))
+
+                              (defn- on-form-submit [js-evt]
+                                (.preventDefault js-evt)
+                                (let [username (dom/get-value "nameInput")]
+                                  (when (valid-username? username)
+                                    (reset! client.state/username username)
+                                    (send-login! username)
+                                    (aset js/location "hash" "#/lobby"))))}))
 
 (defn- add-events! []
   (.on ($ "#loginForm") "submit" on-form-submit))
@@ -60,5 +64,5 @@
 
   (if-not (= 1 @dom/current-panel)
     (dom/animate-to-panel 1 (fn []
-      (.focus (dom/by-id "nameInput"))))
+                             (.focus (dom/by-id "nameInput"))))
     (.focus (dom/by-id "nameInput"))))
